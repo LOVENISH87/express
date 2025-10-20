0fa3cab88f7e2974fe686af87212d577bc712e50
LOVENISH87 <150184006+LOVENISH87@users.noreply.github.com>
2025-10-20 16:22:09 +0530

    something

---
+++ JSon_arr.js
@@ -0,0 +1,20 @@
+// const arr = [
+
+//     { "name" : "a",
+//     "age" : 12,
+//     "price" : "captain" },
+
+
+// ]
+
+
+const arr = ["name" , "place" , "game"]
+
+
+const aws = JSON.parse(arr)
+
+
+console.log(aws)
+// const ans = JSON.stringify(arr)
+
+// console.log(JSON.parse(ans))
---
+++ Json.js
@@ -0,0 +1,37 @@
+// const { name } = require("ejs")
+
+
+
+let a = 45;
+
+const name = {
+    "n": "Dr. J",
+    "age": 85
+}
+
+
+// const game = {
+//     "name": "james",
+//     "class": "DIR",
+//     "isGood": "sometimes Good sometimes Goof"
+// }
+
+
+// const game = {"batman is gOat" : "Ba"}
+
+// const bat = JSON.parse(game)
+// console.log(bat)
+
+const arr = ["a", "b"]
+console.log(JSON.parse(JSON.stringify(typeof (name))))
+console.log(JSON.stringify(typeof (name))) //obj to str
+
+console.log(JSON.parse.stringify(name))
+
+console.log(typeof (a))
+
+
+
+//  console.log(JSON.parse(arr)) not work alone use stringify@@
+
+
