4bb50224f95c7da8b4e52014b20d27d746142e63
LOVENISH87 <150184006+LOVENISH87@users.noreply.github.com>
2025-11-14 14:08:08 +0530

    nothing to commit

--- MiddleWare.js
+++ MiddleWare.js
@@ -5,12 +5,22 @@ const app = express()
 const port = 4800;
 
 
-app.use(function(req, resp , next) {
-    console.log("this is middle ware that runs before any route? i might be wrong");
-    // it gets stuck on some point so use next()
+// app.use(function(req, resp , next) {
+//     console.log("this is middle ware that runs before any route? i might be wrong");
+//     // it gets stuck on some point so use next()
 
+//     next();
+// })
+
+
+// Global mid-ware!!!
+function middleware = (req ,resp , next) =>{
+    resp.send("nice")
     next();
-})
+} 
+
+// console.log()
+middleware();
 
 app.listen(port)
 
